dfbdfe8a219ae6a735d6ba35e02a767a12275ced
KHyunJoong <goesy91@naver.com>
2024-11-05 19:08:14 +0900

    feat: AI server '/judgement'구동확인

--- app/services/situation_summary.py
+++ app/services/situation_summary.py
@@ -2,7 +2,8 @@ def situation_summary_GPT(text):
     return ""
 
 def stt_model(link):
-    return "text"
+    A = {'ai_stt':"stt_test_text"}
+    return A
 
 def generate_response(text):
     return "entities"
