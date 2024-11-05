3b8e3310cba3cb1c1b672ac5daf95664f5c3912a
KHyunJoong <goesy91@naver.com>
2024-11-05 18:30:27 +0900

    feat: AI server '/judgement'구동확인

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = " "
-    DATABASE_URL: str = " "
+    # DATABASE_URL: str = " "
     OPENAI_API_KEY: str = ""
 
     class Config:
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 
-from app.api.endpoint.mediation_service import process_judge, get_voice
+from app.api.endpoint.mediation_service import router as mediation_router
 from app.core.config import settings
 
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-app.include_router(process_judge.router, prefix="/api/v1/ai/private-posts")
-app.include_router(get_voice.router, prefix="/api/v1/ai/private-posts")
+# mediation_router를 포함
+app.include_router(mediation_router, prefix="/api/v1/private-posts")
 
 if __name__ == "__main__":
     import uvicorn
