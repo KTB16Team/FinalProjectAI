0e729af7755c1c7887c3c46f11d9580e75fc96fb
KHyunJoong <goesy91@naver.com>
2024-12-20 00:41:29 +0900

    feat: Mq 소비 수정

--- app/main.py
+++ app/main.py
@@ -5,12 +5,9 @@ from core.config import settings
 import asyncio
 import threading
 from api.endpoint.mediation_service import start_worker
+from contextlib import asynccontextmanager
+from core.logging import logger
 
-app = FastAPI(title=settings.PROJECT_NAME)
-
-# mediation_router를 포함
-app.include_router(mediation_router, prefix="/api/v1/private-posts")
-app.include_router(prometheus_router, prefix="")
 def run_worker_in_thread():
     """
     RabbitMQ 워커를 별도 스레드에서 실행
@@ -19,21 +16,29 @@ def run_worker_in_thread():
     asyncio.set_event_loop(loop)
     start_worker()
 
-if __name__ == "__main__":
-    import uvicorn
 
-    # # RabbitMQ 워커 실행
-    # worker_thread = threading.Thread(target=run_worker_in_thread, daemon=True)
-    # worker_thread.start()
-
-    # FastAPI 서버 실행
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    #임시
-
-@app.on_event("startup")
-async def startup_event():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     """
-    FastAPI 앱 시작 시 RabbitMQ 워커를 별도 스레드에서 실행
+    Lifespan 핸들러로 RabbitMQ 워커 스레드 관리
     """
+    logger.info("Initializing application...")
     worker_thread = threading.Thread(target=run_worker_in_thread, daemon=True)
     worker_thread.start()
+    logger.info("RabbitMQ worker thread started.")
+
+    yield  # 애플리케이션 실행 유지
+
+    logger.info("Shutting down application...")
+    worker_thread.join(timeout=5)  # 스레드 종료 대기
+
+app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
+
+# 라우터 포함
+app.include_router(mediation_router, prefix="/api/v1/private-posts")
+app.include_router(prometheus_router, prefix="")
+
+if __name__ == "__main__":
+    import uvicorn
+    logger.info("FastAPI application is starting...")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
